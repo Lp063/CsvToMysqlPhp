9103fcb4837810c8d29014754c2a8501aad18b94
Lohit <lohit.pereira09@stanislausbandra.in>
2017-04-12 14:28:38 +0530

    Removed comments and unnecessary code

--- lpexcel.php
+++ lpexcel.php
@@ -1,16 +1,3 @@
-<?php
-	$con=mysqli_connect("localhost","root","","lpexceldb");
-	if (mysqli_connect_errno()){						// Check connection
-	  	echo "Failed to connect to MySQL: " .mysqli_connect_error();
-	  }
-	$columH = array();
-	$rowQ="select * from products";
-	$result_set =  mysqli_query($con,$rowQ);
-	//$rowQ="select * from products";
-	//$result_set =  mysqli_query($con,$rowQ);
-	//while($row = mysqli_fetch_array($result_set))
-	//{ echo $row['id'];}
-?>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -39,33 +26,6 @@
 				</fieldset>
 			</form>
 		</div>
-		<!--table class="table table-bordered">
-			<tr>
-				<?php
-					$colsQ = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='lpexceldb' AND `TABLE_NAME`='products'";
-					$columns = mysqli_query($con,$colsQ);
-					while($col = mysqli_fetch_array($columns) ){
-						$columnH[]=$col[0];
-						echo "<th>".$col[0]."</th>";
-					}
-				?>
-			</tr>
-			<?php 
-				//echo"<pre>";print_r($columnH);echo"</pre>";
-				$x=count($columH);
-				echo $x;
-				echo "<tr>";
-				
-					while($row = mysqli_fetch_array($result_set)){
-						for($y=0;$y<$x;$y++){
-						$colval = $columnH[$y];
-							echo "<td>".$row[$colval]."</td>";
-						}
-					}
-				
-				echo "</tr>";
-			?>
-		</table-->
 	</body>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </html>
