579d9ff72a6d055751104e55eed94231f0c6415c
Lohit <lohit.pereira09@stanislausbandra.in>
2017-04-12 14:43:29 +0530

    ToDo: obtain Unique ID for second column of each query

--- lpexcel.php
+++ lpexcel.php
@@ -2,8 +2,6 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Excel to MySQL</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	</head>
 	<style>
 		.uploadbox{
@@ -12,7 +10,7 @@
 		}
 	</style>
 	<body>
-		<div align="center" class="uploadbox well well-sm">
+		<div align="center" class="uploadbox">
 			<form action="lpimport.php" method="post" name="upload_excel" enctype="multipart/form-data">
 				<fieldset>
 				<legend>Import CSV/Excel file</legend>
